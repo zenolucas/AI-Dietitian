43613629efc69fdc76304352a89115ecb84d54d0
Zen Lucas <2200501@slu.edu.ph>
2024-05-12 11:35:06 +0800

    reformat meals.go

--- types/meals.go
+++ types/meals.go
@@ -1,10 +1,12 @@
 package types
 
-
 type Meal struct {
-    MealName        string   `json:"mealName"`
-    ImageFilePath   string   `json:"imageFilePath"`
-    Ingredients     []string `json:"ingredients"`
-    Procedure       []string `json:"procedure"`
-    FriendlyComments string   `json:"friendlyComments"`
+	MealName         string   `json:"name"`
+	ImageFilePath    string   `json:"imageFilePath"`
+	Ingredients      []string `json:"ingredients"`
+	Directions       []string `json:"directions"`
+	PrepTime         string   `json:"prep_time"`
+	CookTime         string   `json:"cook_time"`
+	Servings         string   `json:"servings"`
+	FriendlyComments string   `json:"friendlyComments"`
 }
